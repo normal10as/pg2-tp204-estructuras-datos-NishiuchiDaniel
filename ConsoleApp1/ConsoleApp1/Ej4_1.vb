9ad10f731b368acb7ca8da3cf41b6c7b215b9352
Daniel <Daniel@DESKTOP-ISVANOC>
2018-05-28 20:31:48 -0300

    tp4

---
+++ ConsoleApp1/ConsoleApp1/Ej4_1.vb
@@ -0,0 +1,24 @@
+﻿Module Ej4_1
+
+    Sub Main()
+
+        Dim arreglo(4) As Double
+        Dim sumatoria As Double
+        Dim media As Double
+
+        For x = 0 To arreglo.Length - 1
+            Console.Write("Ingrese valor " & x + 1 & " : ")
+            arreglo(x) = Console.ReadLine()
+            sumatoria += arreglo(x)
+        Next
+
+        media = sumatoria / arreglo.Length
+        Console.WriteLine("La media de los elementos es: " & media)
+        For x = 0 To arreglo.Length - 1
+            Console.WriteLine("La desviacion del elemento " & x & " es: " & arreglo(x) - media)
+        Next
+
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ ConsoleApp1/ConsoleApp1/Ej4_2.vb
@@ -0,0 +1,37 @@
+﻿Module Ej4_2
+    Sub main()
+        Dim codigo = New Byte() {1, 2, 3, 4, 5}
+        Dim nombre = New String() {"pepsi", "brahma", "coca", "lays", "sprite"}
+        Dim preciounitario = New Decimal() {5, 20, 10, 15, 25}
+        Dim cantidad As UInt32
+        Dim codigoingresado As Byte
+        Dim totalgeneral As Decimal
+        Do
+            codigoingresado = Comprobar()
+
+            If codigoingresado <> 0 Then
+                Console.WriteLine("Nombre de producto: " & nombre(codigoingresado - 1))
+                Console.WriteLine("Precio unitario: " & preciounitario(codigoingresado - 1))
+                Console.Write("Ingrese cantidad: ")
+                cantidad = Console.ReadLine()
+                Console.WriteLine("Total del producto " & nombre(codigoingresado - 1) & " es: " & preciounitario(codigoingresado - 1) * cantidad)
+                totalgeneral += preciounitario(codigoingresado - 1) * cantidad
+                Console.WriteLine("Total general: " & totalgeneral)
+            End If
+        Loop Until (codigoingresado = 0)
+
+
+    End Sub
+
+    Friend Function Comprobar() As Byte
+        Dim a As Integer = -1
+        While a < 0 Or a > 4
+            Console.Write("Ingrese codigo entre 1 y 4 y cero para salir: ")
+            a = Console.ReadLine()
+            If a < 0 Or a > 4 Then
+                Console.WriteLine("El valor ingresado es incorrecto")
+            End If
+        End While
+        Return (a)
+    End Function
+End Module
